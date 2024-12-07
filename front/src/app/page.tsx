6c8a4b32789f05ecfa471e2a6033b26b5fb4c7bb
NKGarzonGrajales <karycyber22@hotmail.com>
2024-12-06 20:32:59 -0500

    front/src/app/page.tsx

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,1 +1,12 @@
 //import Image from "next/image";
+import React from 'react'
+
+const Homeview = () => {
+  return (
+    <div>
+      Welcome to our HomePage
+    </div>
+  )
+}
+
+export default Homeview;
