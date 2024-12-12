b317aa46dd2961f1db35f8f9af939fbd6a00cb44
NKGarzonGrajales <karycyber22@hotmail.com>
2024-12-12 16:34:58 -0300

    add email service

--- back/src/auth/auth.module.ts
+++ back/src/auth/auth.module.ts
@@ -3,9 +3,16 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { PrismaService } from 'prisma/prisma.service';
 import { UserService } from '../user/user.service';
+import { EmailModule } from 'src/email/email.module';
 
 @Module({
-  imports: [JwtModule.register({ secret: 'your-secret-key', signOptions: { expiresIn: '1h' } })],
+  imports: [
+    JwtModule.register({
+      secret: 'your-secret-key',
+      signOptions: { expiresIn: '1h' },
+    }),
+    EmailModule,
+  ],
   providers: [AuthService, PrismaService, UserService],
   exports: [AuthService],
 })
