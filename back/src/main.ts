75169fb3672b8932803235205922312f4eeb5548
NKGarzonGrajales <karycyber22@hotmail.com>
2024-12-13 17:27:18 -0300

    config main.ts

--- back/src/main.ts
+++ back/src/main.ts
@@ -5,12 +5,19 @@ import { NextFunction, Request, Response } from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Middleware de Express
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, 
+  });
+
   app.use((req: Request, res: Response, next: NextFunction) => {
-    console.log(`${req.method} ${req.url}`);
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
     next();
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  const PORT = process.env.PORT || 4000;
+  await app.listen(PORT);
+  console.log(`🚀 Backend corriendo en http://localhost:${PORT}/`);
 }
 bootstrap();
