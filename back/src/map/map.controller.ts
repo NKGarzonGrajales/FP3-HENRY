b0a46c96a2f0159a41d940e5b7ee74af97d729b4
NKGarzonGrajales <karycyber22@hotmail.com>
2024-12-19 11:54:39 -0300

    fix src/map

--- back/src/map/map.controller.ts
+++ back/src/map/map.controller.ts
@@ -1,17 +1,9 @@
-import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { MapService } from './map.service';
-import { CreateLocationDto } from './dto/create-location.dto';
 
 @Controller('map')
 export class MapController {
   constructor(private readonly mapService: MapService) {}
-
-
-  @Post('location')
-  async createLocation(@Body() createLocationDto: CreateLocationDto) {
-    return await this.mapService.createLocation(createLocationDto);
-  }
-
 
   @Get('location/:postId')
   async getLocation(@Param('postId') postId: string) {
--- back/src/map/map.service.ts
+++ back/src/map/map.service.ts
@@ -1,25 +1,9 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
-import { CreateLocationDto } from './dto/create-location.dto';
 
 @Injectable()
 export class MapService {
   constructor(private readonly prisma: PrismaService) {}
-
-  async createLocation(createLocationDto: CreateLocationDto) {
-    const { latitude, longitude, address, postId } = createLocationDto;
-
-    const location = await this.prisma.location.create({
-      data: {
-        latitude,
-        longitude,
-        address,
-        post: postId ? { connect: { id: postId } } : undefined,
-      },
-    });
-
-    return location;
-  }
 
   async getLocation(postId: string) {
     const location = await this.prisma.location.findFirst({
