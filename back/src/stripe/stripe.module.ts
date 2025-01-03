63ddbb5bd1409e601d3aa47cd05065257585ee82
NKGarzonGrajales <karycyber22@hotmail.com>
2025-01-03 17:40:31 -0400

    src/stripe.service.ts: incorporación de donación en la base de datos e email automático

--- back/src/stripe/stripe.module.ts
+++ back/src/stripe/stripe.module.ts
@@ -2,16 +2,20 @@ import {Module } from '@nestjs/common';
 import { StripeService } from './stripe.service';
 import { StripeController } from './stripe.controller';
 import { ConfigModule } from '@nestjs/config';
-import { timeout } from 'rxjs';
+import { EmailModule } from 'src/email/email.module';
+import { PrismaService } from 'prisma/prisma.service';
+import { UserModule } from 'src/user/user.module';
+import { DonationModule } from 'src/donation/donation.module';
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, EmailModule, UserModule, DonationModule], 
   providers: [StripeService,
     {
     provide: "STRIPE",
     useValue: new (require('stripe'))(process.env.STRIPE_API_KEY, {apiVersion: '2024-12-18.acacia', timeout: 10000}),
-    }
+    }, PrismaService
   ],
   controllers: [StripeController],
   exports: [StripeService ],
+
 })
 export class StripeModule {}
