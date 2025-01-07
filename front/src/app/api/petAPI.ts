8a23b4bdd1f0cf78c59cab7d47d60d5480d77371
NKGarzonGrajales <karycyber22@hotmail.com>
2025-01-07 17:30:38 -0300

    front/src/app/myposts/page.tsx

--- front/src/app/api/petAPI.ts
+++ front/src/app/api/petAPI.ts
@@ -1,4 +1,4 @@
-import { IpetForm, IUserBack } from "@/interfaces/types";
+import { IpetForm } from "@/interfaces/types";
 import Swal from "sweetalert2";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
@@ -164,39 +164,6 @@ export async function deletePet(petId: string): Promise<void> {
     });
 
     console.error("Error al eliminar la mascota:", errorMessage);
-    throw error;
-  }
-}
-
-// get user by id
-export async function getUserById(id: string): Promise<IUserBack | null> {
-  try {
-    const response = await fetch(`${API_URL}/user/${id}`, {
-      method: "GET",
-      headers: { "Content-Type": "application/json" },
-    });
-
-    if (!response.ok) {
-      throw new Error(`Error ${response.status}: ${response.statusText}`);
-    }
-
-    const data = await response.json();
-    return data || null;
-  } catch (error) {
-    const errorMessage =
-      error instanceof Error ? error.message : "Error al obtener el usuario";
-
-    Swal.fire({
-      icon: "error",
-      iconColor: "rose",
-      text: errorMessage,
-      title: "No se logró obtener el usuario",
-      customClass: {
-        confirmButton:
-          "bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded",
-      },
-    });
-    console.error("Error al obtener el usuario:", errorMessage);
     throw error;
   }
 }
