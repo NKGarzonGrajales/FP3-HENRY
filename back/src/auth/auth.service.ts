f5c094135a290a3963d4ead2d80a69081c1f6468
NKGarzonGrajales <karycyber22@hotmail.com>
2025-01-13 18:44:16 -0300

    se arreglo login y register

--- back/src/auth/auth.service.ts
+++ back/src/auth/auth.service.ts
@@ -7,13 +7,24 @@ export class AuthService {
   constructor(private jwtService: JwtService) {}
 
   generateToken(user: any) {
+    console.log("Generando token para el usuario:", user);
+  
+    if (!user || !(user.id || user.sub)) {
+      console.error("El usuario no tiene un ID válido:", user);
+      throw new Error("Usuario inválido para generar token");
+    }
+  
     const payload = {
       email: user.email,
-      sub: user.id,
+      sub: user.id || user.sub,
       role: user.role,
     };
+  
+    console.log("Payload del token:", payload);
     return this.jwtService.sign(payload);
   }
+  
+  
 
   async validatePassword(
     plainPassword: string,
