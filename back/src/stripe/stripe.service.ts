7021561e02493e748bcee806f8e997d0877dca9e
NKGarzonGrajales <karycyber22@hotmail.com>
2025-01-14 19:11:29 -0400

    actualizada la pasarela de pagos quedo funcionando

--- back/src/stripe/stripe.service.ts
+++ back/src/stripe/stripe.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Inject } from '@nestjs/common';
 import Stripe from 'stripe';
 import { EmailService } from 'src/email/email.service';
 import { PrismaService } from 'prisma/prisma.service';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class StripeService {
@@ -32,12 +33,12 @@ export class StripeService {
         success_url: process.env.STRIPE_SUCCESS_URL,
         cancel_url: process.env.STRIPE_CANCEL_URL,
       });
-      await this.emailService.sendMailWithTemplate(
-        email,
-        'Donación en proceso',
-        { email, amount: amount / 100 },
-        'donationCreation',
-      );
+        await this.emailService.sendMailWithTemplate(
+          email,
+          'Donación en proceso',
+          { email, amount: amount / 100 },
+          'donationCreation',
+        );
 
       return session;
     } catch (error) {
@@ -68,47 +69,76 @@ export class StripeService {
     switch (event.type) {
       case 'checkout.session.completed':
         const session = event.data.object as Stripe.Checkout.Session;
-  
+        console.log('esto ES SESSION', session);
+
         const existingDonation = await this.prisma.donations.findFirst({
           where: { paymentIntent: session.payment_intent as string },
         });
-  
+
         if (existingDonation) {
-          console.log(`El pago con intent ${session.payment_intent} ya fue procesado.`);
+          console.log(
+            `El pago con intent ${session.payment_intent} ya fue procesado.`,
+          );
           return;
         }
-  
-        const user = await this.prisma.user.findUnique({
-          where: { email: session.customer_email },
-        });
-  
-      
-        const userId = user ? user.id : null;
 
-        await this.prisma.donations.create({
-          data: {
-            amount: session.amount_total / 100,
-            email: session.customer_email,
-            userId,
-            paymentIntent: session.payment_intent as string,
-          },
-        });
-  
-        await this.emailService.sendMailWithTemplate(
-          session.customer_email,
-          'Pago de donación exitoso',
-          { amount: session.amount_total / 100 },
-          'donationSuccess',
-        );
+        try {
+          const usuerCheckout = await this.prisma.donations.create({
+            data: {
+              amount: session.amount_total / 100,
+              email: session.customer_email,
+              id: uuidv4(),
+              paymentIntent: session.payment_intent as string,
+            },
+          });
+          console.log('esto es usuerCheckout', usuerCheckout);
+
+          await this.emailService.sendMailWithTemplate(
+            session.customer_email,
+            'Pago de donación exitoso',
+            { amount: session.amount_total / 100 },
+            'donationSuccess',
+          );
+        } catch (error) {
+          console.error('Error al crear donacion', error);
+        }
         break;
-  
+      case 'charge.succeeded':
+        const session1 = event.data.object as Stripe.Charge;
+        console.log('esto ES SESSION1', session1);
+
+        const existingChargeDonation = await this.prisma.donations.findFirst({
+          where: { paymentIntent: session.payment_intent as string },
+        });
+
+        if (existingChargeDonation) {
+          console.log(
+            `El pago con intent ${session.payment_intent} ya fue procesado.`,
+          );
+          return;
+        }
+
+        try {
+          await this.emailService.sendMailWithTemplate(
+            session1.billing_details.email,
+            'Pago de donación exitoso',
+            { amount: session1.amount / 100 },
+            'donationSuccess',
+          );
+        } catch (error) {
+          console.error(
+            'Error al registrar la donacion en charge-sussecced',
+            error,
+          );
+        }
+        break;
+
       case 'payment_intent.payment_failed':
         console.error('PaymentIntent was not successful');
         break;
-  
+
       default:
         console.log(`Unhandled event type ${event.type}`);
     }
   }
-  
 }
