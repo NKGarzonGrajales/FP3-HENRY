e15618e718c374b534550d10c39f428404a8edbc
NKGarzonGrajales <karycyber22@hotmail.com>
2025-01-15 15:44:35 -0300

    fix deletePost

--- front/src/app/api/adminAPIS/getPosts.ts
+++ front/src/app/api/adminAPIS/getPosts.ts
@@ -1,11 +1,25 @@
 import { IPost } from "@/interfaces/types";
+import Cookies from "js-cookie";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const getPosts = async (): Promise<IPost[]> => {
-  const response = await fetch(`${API_URL}/posts`);
+  const token = Cookies.get("token");
+  if (!token) {
+    throw new Error("No se encontró el token de autorización. Inicia sesión nuevamente.");
+  }
+
+  const response = await fetch(`${API_URL}/posts`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`, 
+    },
+  });
+
   if (!response.ok) {
     throw new Error("Error al obtener posts");
   }
+
   return response.json();
 };
