9466a6265c1cfca602f8dc74dadd86585b5a1f53
NKGarzonGrajales <karycyber22@hotmail.com>
2025-01-16 06:25:44 -0500

    front/src/app/donations/page.tsx

--- front/src/app/donations/page.tsx
+++ front/src/app/donations/page.tsx
@@ -4,17 +4,15 @@ import React from "react";
 import Donations from "@/components/Donations/Donations";
 import { IUserSessionDt } from "@/interfaces/types";
 
-const DonationsPage = () => {
-    const userSession: IUserSessionDt = null; 
 
-  return (
-    
-      <Donations userSession={userSession} />
-   
-  );
+const DonationsPage = () => {
+  const userSession: IUserSessionDt | null = null;
+
+  return <Donations userSession={userSession} />;
 };
 
 export default DonationsPage;
+
 
 
 
